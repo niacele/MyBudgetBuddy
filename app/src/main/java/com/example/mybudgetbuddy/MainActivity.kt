371735dceda1cbd2a44bea3f93ab548fdddba384
samanthadeysel <samanthadeysel1@outlook.com>
2026-04-19 20:19:35 +0200

    this is a login tester commit

--- app/src/main/java/com/example/mybudgetbuddy/MainActivity.kt
+++ app/src/main/java/com/example/mybudgetbuddy/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.mybudgetbuddy.ui.theme.MyBudgetBuddyTheme
 
 //tester comment
+//login commit
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
